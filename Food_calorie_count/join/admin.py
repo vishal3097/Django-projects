4136e726a085f0c6098ff025cf102eee31d62cb6
vishal3097 <64057789+vishal3097@users.noreply.github.com>
2021-05-09 15:40:33 -0400

    Add files via upload

---
+++ Food_calorie_count/join/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import User
+from .models import Userinformation
+from .models import Data
+
+@admin.register(User)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display=( 'id','name' ,'email','password','psw')
+
+
+
+admin.site.register(Userinformation)
+
+
+admin.site.register(Data)
+class DataAdmin(admin.ModelAdmin):
+    list_display=('id','exercise','amount')
+
+
